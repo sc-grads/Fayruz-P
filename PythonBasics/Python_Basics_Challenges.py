200dbedeb4a4455fade07ad941d8a896259ad60e
Fayruz <fayruz.paruk@sambeconsulting.com>
2023-02-08 16:05:20 +0200

    adding challenges.py

---
+++ PythonBasics/Python_Basics_Challenges.py
@@ -0,0 +1,68 @@
+# Challenge 1
+a = 5
+b = 5.4
+c = True
+d = 'My name is Fayruz'
+e = [5, 'Amy', 6.2]
+
+print(type(a), a)
+print(type(b), b)
+print(type(c), c)
+print(type(d), d)
+print(type(e), e)
+
+# Challenge 2
+# Change this script so that it follows the Python naming and style conventions described in PEP8.
+my_name = 'Andrei'
+value = 100
+
+# This is
+# an example of a multiline
+# comment in Python.
+
+
+this_str = 'Hello'
+print(this_str)
+
+# Challenge 3
+# This script contains some syntax errors.
+# Modify the script so that it runs without any errors.
+
+best_friend = 'Anne'
+print('My best friend is ', best_friend)
+
+age = 18
+print(age == 10)
+
+a, b = ('10', '20')
+print(a + b)
+
+print('To comment a line of code you # at the beginning of the line.')
+
+
+# Challenge 4
+
+a = 10
+print(a)
+print(a==2)
+print( a >= 100)
+print(a * a)
+print(a ** 3)
+print(100 / a)
+print (25 // a)
+print (12 % a)
+a += 5
+a *= 2
+
+# Challenge 5
+
+a = (16 / (2 + 6) / 2) ** 2
+
+# Challenge 6
+
+# Challenge 7
+rev = 45_897_513
+profit = rev * 12.7 / 100
+print("Company's profit:", format(profit, '.2f'))
+
+
