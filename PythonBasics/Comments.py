ff232d4fdd15762c91a8a3f74d0e681474a9581d
Fayruz <fayruz.paruk@sambeconsulting.com>
2023-02-08 14:01:13 +0200

    Adding Section 1(Python Basics) files.

---
+++ PythonBasics/Comments.py
@@ -0,0 +1,16 @@
+# this is a comment
+
+# a = 5
+
+x = 9 # here I create a variable called x that stores an integer(9)
+
+my_str = 'A hash # withing a string literal is just a hash char.'
+a = 10
+print ('Hello Pyhton!')
+
+"""
+x = 10
+print(x)
+"""
+# dcstrings used for documentation.
+
---
+++ PythonBasics/Operators.py
@@ -0,0 +1,30 @@
+# Arithmetic Operators
+
+# +
+print(5 + 5)
+# -
+print(3 - 9)
+# /
+print(8 / 4)
+
+# //
+print(11 // 2)
+
+# *
+print(5 * 4)
+
+# **
+print(2 ** 100)
+
+# %
+print(8 % 5)
+print(14 % 4)
+
+print(4 * 5.0)
+
+print(2 + 4 * 2 ** 3) # 34
+
+print((2 + 4) * 2 ** 3) # 48
+
+print(1_000_000_000)
+print(1000 == 1_000)
