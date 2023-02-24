a16c14c4317f034b243dc0888bd1f83c7a6f3f51
Fayruz <fayruz.paruk@sambeconsulting.com>
2023-02-24 17:42:24 +0200

    Update

--- TextFiles/absolute_and_relative_paths.py
+++ TextFiles/absolute_and_relative_paths.py
@@ -1,5 +1,5 @@
-f = open('C:\\Users\\Fayruz Paruk\\PycharmProjects\\Fayruz-P\\Fayruz-P\\TextFiles\\settings.txt')
-f = open(r'C:\Users\Fayruz Paruk\PycharmProjects\Fayruz-P\Fayruz-P\TextFiles\settings.txt')
+f = open('/Fayruz-P/PythonBootcamp\\TextFiles\\settings.txt')
+f = open(r'/Fayruz-P/PythonBootcamp\TextFiles\settings.txt')
 
 # . => current working directory
 # .. => parent directory
