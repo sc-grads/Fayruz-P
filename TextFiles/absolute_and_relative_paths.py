5ef50ea4371f065eaee4e95a6fbe88621d8ada74
Fayruz <fayruz.paruk@sambeconsulting.com>
2023-02-24 18:06:30 +0200

    Added PythonBoootcamp Directory

--- TextFiles/absolute_and_relative_paths.py
+++
@@ -1,6 +0,0 @@
-f = open('/Fayruz-P/PythonBootcamp\\TextFiles\\settings.txt')
-f = open(r'/Fayruz-P/PythonBootcamp\TextFiles\settings.txt')
-
-# . => current working directory
-# .. => parent directory
-open('settings.txt')
---
+++ Training/PythonBootcamp/TextFiles/absolute_and_relative_paths.py
@@ -0,0 +1,6 @@
+f = open('/Fayruz-P/Fayruz-P/PythonBootcamp/TextFiles/settings.txt')
+f = open(r'/Fayruz-P/Fayruz-P/PythonBootcamp/TextFiles/settings.txt')
+
+# . => current working directory
+# .. => parent directory
+open('settings.txt')
