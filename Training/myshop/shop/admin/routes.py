55b7b5eb531c1d836ac18f37db0e52a00d1fb311
Fayruz <fayruz.paruk@sambeconsulting.com>
2023-03-09 17:07:31 +0200

    added registration and login files

---
+++ Training/myshop/shop/admin/routes.py
@@ -0,0 +1,40 @@
+from flask import render_template, session, request, redirect, url_for, flash
+
+from shop import app, db, bcrypt
+from .forms import RegistrationForm,LoginForm
+import os
+from .models import User
+
+
+@app.route('/')
+def home():
+    return render_template('admin/index.html',title='Admin Page')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(name=form.name.data, username=form.username.data, email=form.email.data,
+                    password=hash_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Welcome {form.name.data} Thank you for registering','success')
+        return redirect(url_for('home'))
+    return render_template('admin/register.html', form=form, title="Registration Page")
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == "POST" and form.validate():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            session['email'] = form.email.data
+            flash(f'Welcome {form.email.data} You are logedin now' , 'success')
+            return redirect(request.args.get('next') or url_for('admin'))
+        else:
+            flash('Wrong Password, please try again!', 'danger')
+
+
+    return render_template('admin/login.html', form=form,title='Login Page')
