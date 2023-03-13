c64d711b8896fd314e4eeb423109ecbdf8023b8d
Fayruz <fayruz.paruk@sambeconsulting.com>
2023-03-13 13:31:46 +0200

    added data types and functions files

---
+++ Training/QueryingMSSQLWithT-SQL/DataTypesAndFunctions/date_functions.sql
@@ -0,0 +1,7 @@
+SELECT CURRENT_TIMESTAMP as RightNow
+select getdate() as RightNow
+select SYSDATETIME() AS RightNow
+select dateadd(year,1,'2015-01-02 03:04:05') as myYear
+select datepart(hour,'2015-01-02 03:04:05') as myHour
+select datename(weekday, getdate()) as myAnswer
+select datediff(second,'2015-01-02 03:04:05',getdate()) as SecondsElapsed
