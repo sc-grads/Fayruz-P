b713934ab17b0f7f4dfc5331d15302ee5680e25a
Fayruz <fayruz.paruk@sambeconsulting.com>
2023-03-13 12:32:16 +0200

    added string data types and functions files

---
+++ Training/QueryingMSSQLWithT-SQL/StringDataTypesAndFunctions/activity_5.sql
@@ -0,0 +1,19 @@
+--Question 1
+
+select [name] + 'A'
+from sys.all_columns
+
+--Question 2
+
+select [name] + N'?'
+from sys.all_columns
+
+--Question 3
+
+select substring([name],2,len([name])-1) as [name]
+from sys.all_columns
+
+--Question 4
+
+select substring([name],1,len([name])-1) as [name]
+from sys.all_columns
---
+++ Training/QueryingMSSQLWithT-SQL/StringDataTypesAndFunctions/joining_two_strings_together.sql
@@ -0,0 +1,13 @@
+declare @firstname as nvarchar(20)
+declare @middlename as nvarchar(20)
+declare @lastname as nvarchar(20)
+
+set @firstname = 'Sarah'
+--set @middlename = 'Jane'
+set @lastname = 'Milligan'
+
+-- select @firstname + ' ' + @middlename + ' ' + @lastname as FullName
+select @firstname + iif(@middlename is null, '', ' ' + @middlename) + ' ' + @lastname as FullName
+select @firstname + CASE WHEN @middlename IS NULL THEN '' ELSE ' ' + @middlename END + ' ' + @lastname as FullName
+select @firstname + coalesce(' ' + @middlename,'') + ' ' + @lastname as FullName
+SELECT CONCAT(@firstname,' ' + @middlename, ' ' , @lastname) as FullName
