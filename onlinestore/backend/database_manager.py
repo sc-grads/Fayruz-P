100bd89704437176851a33ebcd6d876daa536e2e
Fayruz <fayruz.paruk@sambeconsulting.com>
2023-03-28 16:53:23 +0200

    updated register and login

--- onlinestore/backend/database_manager.py
+++ onlinestore/backend/database_manager.py
@@ -1,4 +1,5 @@
 import pyodbc
+from flask import Flask, request, session, jsonify
 
 server = 'DESKTOP-ISR70S1'
 database = 'Spicelet'
@@ -17,12 +18,38 @@ def conn():
     return 'Connected'
 
 
-def insert(fname, lname, email,newpassword,number ,address):
+def insert(fname, lname, email, newpassword, number, address):
     dbconnect = pyodbc.connect(connection)
     cursor = dbconnect.cursor()
-    cursor.execute(f"Insert into Customers(first_name,last_name,email_address,password,contact_no,address) VALUES('{fname}', '{lname}', '{email}','{newpassword}','{number}' ,'{address}')")
+    cursor.execute(
+        f"Insert into Customers(first_name,last_name,email_address,password,contact_no,address) VALUES('{fname}', '{lname}', '{email}','{newpassword}','{number}' ,'{address}')")
     cursor.commit()
     cursor.close()
     return 'Added User'
 
-# insert('Fayruz','Paruk','fayruz.p@gmail.com','12345','0792480727','12 Bird Street')
+def retrieve(username, newpassword):
+    dbconnect = pyodbc.connect(connection)
+    cursor = dbconnect.cursor()
+    cursor.execute('SELECT * FROM Customers WHERE email_address = ? AND password = ?', (username, newpassword))
+    row = cursor.fetchone()
+    if row:
+        # TODO: generate and return a token or session variable
+        session['customer_ID'] = row.id
+        return jsonify({'success': True, 'message': 'Logged in'})
+    else:
+        return jsonify({'success': False, 'message': 'Invalid username or password'})
+
+
+def profile():
+    user_id = session.get('customer_ID_id')
+
+    if user_id is None:
+        return 'Unauthorized', 401
+    else:
+        dbconnect = pyodbc.connect(connection)
+        cursor = dbconnect.cursor()
+        cursor.execute('SELECT * FROM Customers WHERE id = ?', (user_id,))
+        user = cursor.fetchone()
+        cursor.close()
+
+        return jsonify({'username': user.username, 'email': user.email})
--- onlinestore/backend/routes.py
+++ onlinestore/backend/routes.py
@@ -1,23 +1,34 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from flask_cors import CORS
+from pip._internal.network import session
+
 import database_manager as dbm
+from flask_session import Session
 
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:4200'])
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SECRET_KEY'] = 'AbY&FaY1008'
+Session(app)
+@app.route('/set-customer-profile', methods=['POST'])
+def set_customer_profile():
+    data = request.get_json()
+    session['customer_profile'] = data
+    return 'Customer profile set.'
 
+@app.route('/get-customer-profile', methods=['GET'])
+def get_customer_profile():
+    return session.get('customer_profile', {})
 
 @app.route('/login', methods=['POST'])
 def login():
-    userinfo = request.json
-    user = userinfo.get('username')
-    password = userinfo.get('password')
-    print(user)
-    print(password)
-    isconnected = dbm.conn()
-    return {'Status': isconnected}
+    username = request.json.get('username')
+    password = request.json.get('password')
+    dbm.retrieve(username, password)
+    return {'Status': 'User Logged in'}
 
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     registerinfo = request.json
     fname = registerinfo.get('fname')
@@ -26,8 +37,13 @@ def register():
     password = registerinfo.get('password')
     number = registerinfo.get('number')
     address = registerinfo.get('address')
-    dbm.insert(fname,lname,email,password,number,address)
-    return {'Status': 'User Registered'}
+    dbm.insert(fname, lname, email, password, number, address)
+    return {'200': 'User Registered and Redirected'}
+
+@app.route('/dashboard')
+def dashboard():
+    return None
+
 
 
 
